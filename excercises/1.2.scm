67f80e08c8e89e89d16f1bdab50e0fc12d75c6cc
Ron Gierlach <rongierlach@gmail.com>
2017-02-09 23:59:47 -0600

    better formatting

---
+++ excercises/1.2.scm
@@ -0,0 +1,10 @@
+;; https://mitpress.mit.edu/sicp/full-text/book/book-Z-H-10.html#%_sec_1.1.2
+(/ (+ 5
+      4
+      (- 2
+         (- 3
+            (+ 6
+               (/ 4 3)))))
+   (* 3
+      (- 6 2)
+      (- 2 7)))
