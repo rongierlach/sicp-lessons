34519bcdca176815bebe2be7dcf2727d8b0b5ee9
Ron Gierlach <rongierlach@gmail.com>
2017-02-10 19:25:52 -0600

    refactor with lexical scoping

--- excercises/1.8.scm
+++ excercises/1.8.scm
@@ -2,11 +2,7 @@
 ;; Newton's method for cube roots is based on the fact that if y is an approximation to the cube root of x,
 ;; then a better approximation is given by the value:
 ;; ((x / y^2) + 2y) / 3
-(define tolerance 0.001)
-
 (define (square x) (* x x))
-
-(define (cube x) (* x x x))
 
 (define (abs x)
   (if (< x 0) (- x) x))
@@ -19,18 +15,17 @@
         (* 2 y))
      3))
 
-(define (improve guess x)
-  (avg guess (newtons-method x guess)))
-
-(define (good-enough? guess x)
-  (< (abs (- (cube guess) x))
-     tolerance))
-
-(define (cube-root-iter guess x)
-  (if (good-enough? guess x)
-      guess
-      (cube-root-iter (improve guess x)
-                      x)))
-
-(define (cube-root x)
-  (cube-root-iter 1.0 x))
+(define (cbrt x)
+  (define tolerance 0.00001)
+  (define initial-guess 1.0)
+  (define (improve guess)
+    (avg guess (newtons-method x guess)))
+  (define (good-enough? guess)
+    (< (/ (abs (- guess (improve guess)))
+          guess)
+       tolerance))
+  (define (cbrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (cbrt-iter (improve guess))))
+  (cbrt-iter initial-guess))
