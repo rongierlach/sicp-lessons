4c2d76395f286411244ac1e311ced2ae9ec95e80
Ron Gierlach <rongierlach@gmail.com>
2017-02-16 16:54:35 -0600

    Excercise 1.11

---
+++ excercises/1.11.scm
@@ -0,0 +1,26 @@
+;; A function f is defined by the rule that:
+;; f(n) = n if n<3 and f(n) = f(n - 1) + 2*f(n - 2) + 3*f(n - 3) if n >= 3.
+;; Write a procedure that computes f by means of a recursive process.
+;; Write a procedure that computes f by means of an iterative process.
+
+;; recursive
+(define (f-recursive n)
+  (if (< n 3)
+      n
+      (+ (f (- n 1))
+         (* 2 (f (- n 2)))
+         (* 3 (f (- n 3))))))
+
+;; iterative
+(define (f-iter a b c count)
+  (if (< count 3)
+      a
+      (f-iter (+ a (* 2 b) (* 3 c))
+      a
+      b
+      (- count 1))))
+
+(define (f n)
+  (if (< n 3)
+      n
+      (f-iter 2 1 0 n)))
